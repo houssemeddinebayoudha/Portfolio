e5fc402b5e94c889d7b05233e9f3dbb0b3d44296
houssemeddinebayoudha <houssemeddine.bayoudha@esprit.tn>
2024-10-05 17:40:29 +0100

    removing the loader

--- js/main.js
+++ js/main.js
@@ -12,17 +12,7 @@
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */ 
-   $(window).load(function() {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      });       
-
-  	})
 
 
   	/*---------------------------------------------------- */
